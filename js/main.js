e77c6da2dbd635b0f9cf24926085397fa75fd0cd
Gianpaolo <pao.barga@gmail.com>
2021-02-18 17:23:29 +0100

    header-contact-nav fatta; iniziato header-content

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 new Vue({
     el: "#root",
     data: {
-    // contact-nav
-    // header-nav
-    
+        openingHours: {
+            time: "9:00 - 18:00",
+            days: "Mon - Sat",
+        }, 
+
+        headerContacts: {
+            tel: "+1 (305) 1234-5678", // ?? va bene o array con icona e dato?
+            email: "hello@example.com",
+            facebook: "facebook",
+            twitter: "twitter",
+            linkedin: "linkedin",    
+        },
+
+        headerContentNav: ["home", "about", "services", "process", "testimonials"],
 
     },
 })
